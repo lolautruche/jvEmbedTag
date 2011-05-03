d5d9540f999049c169175b0f70be349513b8cf06
Jérôme Vieilledent <lolautruche@gmail.com>
2011-05-03 14:25:53 +0200

    Added extension skeleton

---
+++ ezinfo.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * jvEmbedTag ezinfo file
+ * @copyright Copyright (C) 2011 - Jérôme Vieilledent. All rights reserved
+ * @licence http://www.gnu.org/licenses/gpl-2.0.txt GNU GPLv2
+ * @author Jerome Vieilledent
+ * @package metalfrance
+ */
+
+class metalfranceInfo
+{
+    public static function info()
+    {
+        return array(
+            'Name' => 'jvEmbedTag',
+            'Version' => '@@@VERSION@@@',
+            'Copyright' => 'Copyright (C) 2011 <a href="http://www.lolart.net">Lolart.net</a>',
+            'Authors' => 'Jérôme Vieilledent',
+            'License' => 'GNU General Public License v2.0'
+        );
+    }
+}
+
+?>
---
+++ settings/content.ini.append.php
@@ -0,0 +1,13 @@
+<?php /* #?ini charset="utf-8"?
+
+[CustomTagSettings]
+AvailableCustomTags[]=youtube
+CustomTagsDescription[youtube]=Youtube Video
+
+[youtube]
+CustomAttributes[]
+CustomAttributes[]=presentation_url
+CustomAttributes[]=width
+CustomAttributes[]=height
+
+*/ ?>
