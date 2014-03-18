c180e1710e085fae1ab761736b84203717871826
Jérôme Vieilledent <lolautruche@gmail.com>
2014-03-18 10:28:39 +0100

    Switched to MIT license

--- ezinfo.php
+++ ezinfo.php
@@ -1,10 +1,7 @@
 <?php
 /**
  * jvEmbedTag ezinfo file
- * @copyright Copyright (C) 2011 - Jérôme Vieilledent. All rights reserved
- * @licence http://www.gnu.org/licenses/gpl-2.0.txt GNU GPLv2
  * @author Jerome Vieilledent
- * @package metalfrance
  */
 
 class jvembedtagInfo
@@ -14,9 +11,9 @@ class jvembedtagInfo
         return array(
             'Name' => 'jvEmbedTag',
             'Version' => '@@@VERSION@@@',
-            'Copyright' => 'Copyright (C) 2011 <a href="http://www.lolart.net">Lolart.net</a>',
+            'Copyright' => 'Copyright © 2011-' . date( 'Y' ) . ' <a href="http://www.lolart.net">Lolart.net</a>',
             'Authors' => 'Jérôme Vieilledent',
-            'License' => 'GNU General Public License v2.0'
+            'License' => 'MIT'
         );
     }
 }
